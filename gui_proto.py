293cda35a9eaadf417e2de161f18e6d4671f8d19
Carson Sears <searscr@ornl.gov>
2024-02-28 16:40:29 -0600

    Cleanup

--- gui_proto.py
+++ gui_proto.py
@@ -4,20 +4,20 @@ from PyQt5.QtGui import QDoubleValidator
 
 INVALID_QLINEEDIT = """
 QLineEdit {
-border-color: red;
-border-style: outset;
-border-width: 2px;
-border-radius: 4px;
-padding-left: -1px;
-padding-right: -1px;
-padding-top: 1px;
-padding-bottom: 1px;
+    border-color: red;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 4px;
+    padding-left: -1px;
+    padding-right: -1px;
+    padding-top: 1px;
+    padding-bottom: 1px;
 }
 """
 
 REQUIRED_QLINEEDIT = """
 QLineEdit {
-background-color: lightyellow;
+    background-color: lightyellow;
 }
 """
 
@@ -29,31 +29,27 @@ class SimpleGUI(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.required_items = []
-        self.error_text = {}
-
         # Create QLineEdit fields
-        self.lineEdit1 = QLineEdit(self)
-        self.lineEdit2 = QLineEdit(self)
-        self.lineEdit3 = QLineEdit(self)
-        self.lineEdit3.setPlaceholderText('e.g. x,y,z')
-        self.lineEdit1.setToolTip('Enter the name of a reduction plan.\n'
-                                  'if the name already exists, the existing plan will be overwritten.')
-        self.lineEdit2.setToolTip('Set the wavelength (in Å) for the PeaksWorkspace,\nthe goniometer'
+        self.plan_name = QLineEdit(self)
+        self.wavelength = QLineEdit(self)
+        self.symmetry_operations = QLineEdit(self)
+        self.symmetry_operations.setPlaceholderText('e.g. x,y,z')
+        self.plan_name.setToolTip('Enter the name of a reduction plan.\n'
+                                  'If the name already exists, the existing plan will be overwritten.')
+        self.wavelength.setToolTip('Set the wavelength (in Å) for the PeaksWorkspace,\nthe goniometer'
                                   ' rotation will be calculated when the createPeak method is used.')
-        self.lineEdit3.setToolTip(
+        self.symmetry_operations.setToolTip(
             "List of symmetry operations, or a point/space group symbol."
             "\nSee MDNorm algorithm documentation for more information."
         )
         self.status_label = QLabel('', self)
-        
 
-        self.set_required_style(self.lineEdit1)
-        self.set_required_style(self.lineEdit2)
+        self.set_required_style(self.plan_name)
+        self.set_required_style(self.wavelength)
 
-        self.lineEdit2.defaultValue = 1.486
-        self.lineEdit2.setValidator(QDoubleValidator(0, 100, 4))
-        self.lineEdit2.setText(str(self.lineEdit2.defaultValue))
+        self.wavelength.defaultValue = 1.486
+        self.wavelength.setValidator(QDoubleValidator(0, 100, 4))
+        self.wavelength.setText(str(self.wavelength.defaultValue))
 
         # Create QPushButton (initially disabled)
         self.button = QPushButton('Run Reduction', self)
@@ -63,70 +59,66 @@ class SimpleGUI(QWidget):
         # Create QStatusBar
         self.status_bar = QStatusBar()
         self.status_bar.addWidget(self.status_label)
-        # self.status_bar.setStyleSheet('QStatusBar{border-top: 1px solid #000000;}')
         self.status_bar.setFixedWidth(600)
-
+        self.error_text = {}
 
         # Connect textChanged signals to update button state
-        self.lineEdit1.textChanged.connect(self.update_name_le)
-        self.lineEdit2.textChanged.connect(self.update_wavelength)
-        self.lineEdit3.textChanged.connect(self.update_symmetry_operations)
+        self.plan_name.textChanged.connect(self.update_plan_name)
+        self.wavelength.textChanged.connect(self.update_wavelength)
+        self.symmetry_operations.textChanged.connect(self.update_symmetry_operations)
 
         # create labels
         label1 = QLabel('Reduction Plan Name *', self)
         label2 = QLabel('Wavelength *', self)
         label3 = QLabel('Symmetry Operations', self)
 
-        
-        # self.setLayout(vbox)
         form_layout = QFormLayout()
-        form_layout.addRow(label1, self.lineEdit1)
-        form_layout.addRow(label2, self.lineEdit2)
-        form_layout.addRow(label3, self.lineEdit3)
+        form_layout.addRow(label1, self.plan_name)
+        form_layout.addRow(label2, self.wavelength)
+        form_layout.addRow(label3, self.symmetry_operations)
         form_layout.addRow(self.button)
         form_layout.addRow(self.status_bar)
 
         self.setLayout(form_layout)
 
-        self.setWindowTitle('Simple PyQt GUI')
+        self.setWindowTitle('Demo PyQT Validation GUI')
         self.show()
 
     def update_symmetry_operations(self):
-        self.update_status({self.lineEdit3: '', self.button: ''})
+        self.update_status({self.symmetry_operations: '', self.button: ''})
 
-    def update_name_le(self):
-        if self.lineEdit1.text() == '':
-            self.set_invalid_style(self.lineEdit1)
+    def update_plan_name(self):
+        text1 = self.plan_name.text()
+        if not text1:
+            self.set_invalid_style(self.plan_name)
             error_text = 'Reduction Plan Name: Name is required.'
+        elif len(text1) < 6:
+            self.set_invalid_style(self.plan_name)
+            error_text = 'Reduction Plan Name: Name must be at least 6 characters long.'
         else:
-            if len(self.lineEdit1.text()) < 6:
-                self.set_invalid_style(self.lineEdit1)
-                error_text = 'Reduction Plan Name: Name must be at least 6 characters long.'
-            else:
-                self.set_valid_sytle(self.lineEdit1)
-                error_text = ''
-        self.update_status({self.lineEdit1: error_text})
+            self.set_valid_style(self.plan_name)
+            error_text = ''
+        self.update_status({self.plan_name: error_text})
 
     def update_wavelength(self):
-        text2 = self.lineEdit2.text()
-        fl2 = self.lineEdit2.defaultValue
+        text2 = self.wavelength.text()
         try:
             fl2 = float(text2)
         except ValueError:
-            pass
+            fl2 = self.wavelength.defaultValue
 
-        if (fl2 != self.lineEdit2.defaultValue and fl2 > 5) or text2 == '':
-            self.set_invalid_style(self.lineEdit2)
-            error_text = f"Wavelength: Must be less than 5 Current Value ({self.lineEdit2.text()})."
+        if fl2 != self.wavelength.defaultValue and fl2 > 5 or not text2:
+            self.set_invalid_style(self.wavelength)
+            error_text = f"Wavelength: Must be less than 5. Current Value ({text2})."
         else:
-            self.set_valid_sytle(self.lineEdit2)
+            self.set_valid_style(self.wavelength)
             error_text = ''
-        self.update_status({self.lineEdit2: error_text})
+        self.update_status({self.wavelength: error_text})
 
     def set_invalid_style(self, item):
         item.setStyleSheet(INVALID_QLINEEDIT)
 
-    def set_valid_sytle(self, item):
+    def set_valid_style(self, item):
         item.setStyleSheet('')
 
     def set_required_style(self, item):
@@ -137,45 +129,33 @@ class SimpleGUI(QWidget):
         self.update_button_state(has_invalid)
 
     def update_button_state(self, has_invalid):
-        if has_invalid:
-            self.button.setEnabled(False)
-        else:
-            self.button.setEnabled(True)
+        self.button.setEnabled(not has_invalid)
 
     def update_status_label(self, error_text=None):
-        # Update self.error_text with the incoming error_text
         if error_text is not None:
             for entry in error_text.keys():
                 self.error_text[entry] = error_text[entry]
 
-        # Remove items where the value is ''
         self.error_text = {k: v for k, v in self.error_text.items() if v != ''}
-
-        # Check if any values in self.error_text are non-empty
         has_invalid = any(self.error_text.values())
-
-        # Set the text of the status label
         self.status_label.setText('\n'.join(self.error_text.values()))
-
         return has_invalid
 
     def print_text(self):
-        
-        text3 = self.lineEdit3.text()
+        text3 = self.symmetry_operations.text()
         if text3 != '' and len(text3.split(',')) < 3:
-            self.set_invalid_style(self.lineEdit3)
+            self.set_invalid_style(self.symmetry_operations)
             error_text = 'Symmetry Operations: Must be a valid list of symmetry operations.'
             self.update_status({self.button: error_text})
-            messageBox = QMessageBox()
-            messageBox.setIcon(QMessageBox.Warning)
-            messageBox.setWindowTitle('Invalid Symmetry Operations')
-            messageBox.setText('Symmetry Operations: Must be a valid list of symmetry operations.')
-            messageBox.setStandardButtons(QMessageBox.Ok)
-            messageBox.exec()
+            message_box = QMessageBox()
+            message_box.setIcon(QMessageBox.Warning)
+            message_box.setWindowTitle('Invalid Symmetry Operations')
+            message_box.setText('Symmetry Operations: Must be a valid list of symmetry operations.')
+            message_box.setStandardButtons(QMessageBox.Ok)
+            message_box.exec()
         else:
-            self.set_valid_sytle(self.lineEdit3)
+            self.set_valid_style(self.symmetry_operations)
             self.update_status()
-
 
 
 if __name__ == '__main__':
